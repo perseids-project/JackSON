c36064869c99c26696f599056d2ffa7c03ed9f49
caesarfeta <adamtavares@gmail.com>
2014-09-17 15:28:21 -0400

    Experimenting with JSON-LD.  Starting work on thesaurus prototype before full lexical inventory.  I understand the requirements of a thesaurus better, and the fundamental technical challenges will be explored equally well.

--- test/test_rdf.rb
+++ test/test_rdf.rb
@@ -5,22 +5,29 @@ require_relative 'JackTEST.rb'
 
 # ruby test_rdf.rb --name test_AAB_post_extra
 # ruby test_rdf.rb --name test_AAC_post_list
+# ruby test_rdf.rb --name test_AAD_post_id
 
 class TestRDF < JackTEST
   
   # Create a brand new JSON-LD file
   def test_AAA_post
-    r = api( POST, 'ld', 'rdf/ld' );
+    r = api( POST, 'ld', 'rdf/ld' )
     assert( true )
   end
   
   def test_AAB_post_extra
-    r = api( POST, 'ld_extra', 'rdf/ld' );
+    r = api( POST, 'ld_extra', 'rdf/ld' )
     assert( true )
   end
   
   def test_AAC_post_list
     r = api( POST, 'ld_list', 'rdf/ld_list' )
+    assert( true )
+  end
+  
+  def test_AAD_post_id
+#    r = api( POST, 'cheesy', 'rdf/ld_id/cheesy' )
+    r = api( POST, 'tacky', 'rdf/ld_id/tacky' )
     assert( true )
   end
     
