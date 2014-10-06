308c2a4c212b1a371c81e080181e4fb01be65c8a
caesarfeta <adamtavares@gmail.com>
2014-10-06 15:28:24 -0400

    Started validation unit tests.

---
+++ test/test_validate.rb
@@ -0,0 +1,15 @@
+require_relative 'JackTEST.rb'
+
+class TestValidate < JackTEST
+  
+  # Create a thousand JSON files in under 5 seconds.
+  def test_AAA_context
+  end
+  
+  def test_AAB_type
+  end
+  
+  def test_AAB_regex
+  end
+  
+end
