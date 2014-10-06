c5955caae675b2efb4f9233ecfaf52a6152a16ac
caesarfeta <adamtavares@gmail.com>
2014-10-06 15:46:45 -0400

    Preparing to write validators.

--- test/test_speed.rb
+++ test/test_speed.rb
@@ -4,10 +4,12 @@ class TestSpeed < JackTEST
   
   # Create a thousand JSON files in under 5 seconds.
   def test_AAA_post_thousand
+    assert( true )
   end
   
   # Retrieve a thousand JSON files in under 5 seconds.
   def test_AAB_get_thousand
+    assert( true )
   end
   
 end
--- test/test_validate.rb
+++ test/test_validate.rb
@@ -2,7 +2,6 @@ require_relative 'JackTEST.rb'
 
 class TestValidate < JackTEST
   
-  # Create a thousand JSON files in under 5 seconds.
   def test_AAA_context
   end
   
